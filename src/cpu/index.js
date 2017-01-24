170080e6a398362c126a2b01517af582d5b5ead2
bokuweb <bokuweb12@gmail.com>
2017-01-24 09:45:53 +0900

    Add sample rom

--- src/cpu/index.js
+++ src/cpu/index.js
@@ -80,7 +80,8 @@ export default class Cpu {
       ...defaultRegistors,
       P: { ...defaultRegistors.P }
     };
-    this.registors.PC = this.read(0xFFFC, "Word");
+    // HACK: For rom, not set reset handler.
+    this.registors.PC = this.read(0xFFFC, "Word") || 0x8000;
     log.debug(`pc = ${(this.registors.PC).toString(16)}`);
   }
 
@@ -586,13 +587,15 @@ export default class Cpu {
   }
 
   exec(): number {
-    // const { PC, SP, A, X, Y, P } = this.registors;
-    //log.debug(`PC = ${PC.toString(16)}, SP = ${SP}, A = ${A}, X = ${X} , Y = ${Y}`);
-    // log.debug(`carry = ${P.carry.toString()}, zero = ${P.zero.toString()}, negative = ${P.negative.toString()}, overflow = ${P.overflow.toString()}`);
+    const { PC, SP, A, X, Y, P } = this.registors;
+    if (SP < 0) debugger;
+    log.debug(`PC = ${PC.toString(16)}, SP = ${SP}, A = ${A}, X = ${X} , Y = ${Y}`);
+    log.debug(`carry = ${P.carry.toString()}, zero = ${P.zero.toString()}, negative = ${P.negative.toString()}, overflow = ${P.overflow.toString()}`);
     const opecode = this.fetch(this.registors.PC);
-    const { /* fullName, */ baseName, mode, cycle } = this.opecodeList[opecode];
+    const { baseName, mode, cycle } = this.opecodeList[opecode];
+    const { fullName} = this.opecodeList[opecode];
     const { addrOrData, additionalCycle } = this.getAddrOrDataAndAdditionalCycle(mode);
-    // log.debug(`fullName = ${fullName}, baseName = ${baseName}, mode = ${mode}, cycle = ${cycle}`);
+    log.debug(`fullName = ${fullName}, baseName = ${baseName}, mode = ${mode}, cycle = ${cycle}`);
     this.execInstruction(baseName, addrOrData, mode);
     return cycle + additionalCycle + (this.hasBranched ? 1 : 0);
   }
