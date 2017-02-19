d7eb656ae4f616dc87ccb7fa51517432411fb794
bokuweb <bokuweb12@gmail.com>
2017-02-19 20:39:12 +0900

    Fix keypad type annotation

--- src/keypad/index.js
+++ src/keypad/index.js
@@ -4,16 +4,24 @@ import KeyEvents from './key-events';
 import type { Byte } from '../types/common';
 
 export default class Keypad {
+
+  onKeyDown: (index: number) => void;
+  onKeyUp: (index: number) => void;
+  keyEvents: KeyEvents;
+  isSet: boolean;
+  index: number;
+  keyBuffer: Array<boolean>;
+  keyRegistors: Array<boolean>;
+
   constructor() {
     this.onKeyDown = this.onKeyDown.bind(this);
     this.onKeyUp = this.onKeyUp.bind(this);
     this.keyEvents = new KeyEvents();
     this.keyEvents.setup(this.onKeyDown, this.onKeyUp);
     this.isSet = false;
-    // this.canRead = false;
     this.index = 0;
-    this.keyBuffer = new Array(8);
-    this.keyRegistors = new Array(8);
+    this.keyBuffer = [];
+    this.keyRegistors = [];
   }
 
   onKeyDown(index: number) {
@@ -22,21 +30,19 @@ export default class Keypad {
 
   onKeyUp(index: number) {
     this.keyBuffer[index] = false;
-  }  
+  }
 
   write(data: Byte) {
     if (data & 0x01) {
       this.isSet = true;
-      // this.canRead = false;
     } else if (this.isSet && !(data & 0x01)) {
-      // this.canRead = true;
       this.isSet = false;
       this.index = 0;
       this.keyRegistors = [...this.keyBuffer];
     }
   }
 
-  read(): Byte {
+  read(): boolean {
     return this.keyRegistors[this.index++];
   }
 }
