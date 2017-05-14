bc1779b6b95df06fa28fc38a8e6cf893b7bfccd8
bokuweb <bokuweb12@gmail.com>
2017-05-14 19:58:03 +0900

    Hello

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import { NES } from './nes';
 
-fetch('./static/roms/giko013.nes')
+fetch('./static/roms/hello.nes')
     .then((res) => res.arrayBuffer())
     .then((nesFile: ArrayBuffer) => {
         const nes = new NES();
