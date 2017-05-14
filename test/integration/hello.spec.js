de1ec3fb82414bebf5f9301ea1be9ad87bee3885
bokuweb <bokuweb12@gmail.com>
2017-05-14 20:22:26 +0900

    Add 005

--- test/integration/hello.spec.js
+++ test/integration/hello.spec.js
@@ -21,17 +21,16 @@ describe('integration hello spec', () => {
       screenshot('screenshot/actual/hello.png').then(done);
     }, 1000);
   });
-  
 
-  // it('should render sprite', (done) => {
-  //   const fs = window.require('fs');
-  //   const nesFile = fs.readFileSync('static/roms/giko005.nes');
-  //   const nes = new NES();
-  //   nes.setup(nesFile);
-  //   nes.start();
-  //   setTimeout(() => {
-  //     screenshot('screenshot/actual/giko005.png').then(done);
-  //   }, 1000);
-  // });  
+  it('should render sprite', (done) => {
+    const fs = window.require('fs');
+    const nesFile = fs.readFileSync('static/roms/giko005.nes');
+    const nes = new NES();
+    nes.setup(nesFile);
+    nes.start();
+    setTimeout(() => {
+      screenshot('screenshot/actual/giko005.png').then(done);
+    }, 1000);
+  });  
 });
 
