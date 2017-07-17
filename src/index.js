9ded0e9d951e61e433158d6b11062cfcded02fa4
bokuweb <bokuweb12@gmail.com>
2017-07-17 21:42:06 +0900

    Fix bug, push stack addr is wrong

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import { NES } from './nes';
 
-fetch('./static/roms/nestest.nes')
+fetch('./static/roms/color_test.nes')
     .then((res) => res.arrayBuffer())
     .then((nesFile: ArrayBuffer) => {
         const nes = new NES();
