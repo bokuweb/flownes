b8014bb8eff5ebefc52f9b07295c73d7827117be
bokuweb <bokuweb@users.noreply.github.com>
2017-08-12 21:56:00 +0900

    Update test (#24)
    * Update README
    
    * Add screenshot
    
    * Update test
    
    * Add test
    
    * Add 018nes

---
+++ test/screenshot/screenshot.spec.js
@@ -0,0 +1,90 @@
+import test from 'ava';
+import { screenshot } from 'avaron';
+import { NES } from '../../src/nes';
+
+test.beforeEach(() => {
+  document.body.innerHTML = '<canvas id="nes" width="400" height="300"></canvas>';
+});
+
+test.afterEach(() => {
+  document.body.innerHTML = '';
+});
+
+test.serial('should render hello world', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/hello.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    nes.close();
+    screenshot('screenshot/actual/hello.png').then(done);
+  }, 500)))();
+  t.pass();
+});
+
+test.serial('should render sprite', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/giko005.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    screenshot('screenshot/actual/giko005.png').then(done);
+    nes.close();
+  }, 500)))();
+  t.pass();
+});
+
+test.serial('should render road game', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/giko016.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    screenshot('screenshot/actual/giko016.png').then(done);
+    nes.close();
+  }, 500)))();
+  t.pass();
+});
+
+test.serial('should render horizontal scroll game', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/giko017.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    screenshot('screenshot/actual/giko017.png').then(done);
+    nes.close();
+  }, 500)))();
+  t.pass();
+});
+
+test.serial('should render 018', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/giko018.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    screenshot('screenshot/actual/giko018.png').then(done);
+    nes.close();
+  }, 500)))();
+  t.pass();
+});
+
+test.serial('should render color bar', async t => {
+  const fs = window.require('fs');
+  const nesFile = fs.readFileSync('static/roms/color-bars-mapper0.nes');
+  const nes = new NES();
+  nes.setup(nesFile);
+  nes.start();
+  await (() => new Promise((done) => setTimeout(() => {
+    screenshot('screenshot/actual/color-bars-mapper0.png').then(done);
+    nes.close();
+  }, 500)))();
+  t.pass();
+});
+
