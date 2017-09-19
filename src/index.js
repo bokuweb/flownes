ce800af5005358d20dcc715929182c25e900a90b
bokuweb <bokuweb@users.noreply.github.com>
2017-09-19 21:46:44 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import { NES } from './nes';
 
-fetch('./static/roms/mario.nes')
+fetch('./static/roms/nestest.nes')
     .then((res) => res.arrayBuffer())
     .then((nesFile: ArrayBuffer) => {
         const nes = new NES();
